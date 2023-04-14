e3d6838331138f3f534151480223eeeba621d6e6
“rohithgopala44” <rohithgopala44@gmail.com>
2023-04-14 08:22:51 +0530

    Added: Programm To Remove Details Of A user Manually

---
+++ CurdOperation.JS
@@ -0,0 +1,15 @@
+var selected = null;
+function onFormSubmit(){
+
+}
+
+function readFormData(){
+    var formData = {};
+    formData["Name"] = document.getElementById("Name").value;
+    formData["Gender"] = document.getElementById("Gender").value;
+    formData["Department"] = document.getElementById("Department").value;
+    formData["Salary"] = document.getElementById("salary").value;
+    formData["StartDate"] = document.getElementById("StartDate").value;
+
+
+}
